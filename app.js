b5d1c836a860754b9625f797bf29b932a29bfd5c
ChiatzenW <tokashirizard@gmail.com>
2021-12-22 13:05:42 -0500

    use heroku env port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 const connect = mysql.createConnection({
     host: 'us-cdbr-east-05.cleardb.net',
     user: 'b5ba86357c201e',
